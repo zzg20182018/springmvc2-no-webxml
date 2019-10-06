de9baba455a6010915797d2f674e812a960ec2c5
672819398@qq.com <shandian757898>
2019-10-06 17:14:17 +0800

    Initial commit

---
+++ src/main/java/com/WebAppConfig.java
@@ -0,0 +1,9 @@
+package com;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan("com")
+public class WebAppConfig {
+}
